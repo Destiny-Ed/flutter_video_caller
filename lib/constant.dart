6d6bf8a006edd05443329d5a383c75b27cf886c3
Destiny Ed <destinyed@Destinys-MacBook-Pro.local>
2023-07-19 06:10:20 +0100

    Removed appId

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,2 +1,5 @@
-const appSignId = 'b3d26938f4e62960e872b6b64233205ff53aea922f55621d889b7898deb2a3a0';
-const appId = 1306448147;
+const appSignId = '';
+const appId = '';
+
+
+//Get your id from [https://www.zegocloud.com/] console
